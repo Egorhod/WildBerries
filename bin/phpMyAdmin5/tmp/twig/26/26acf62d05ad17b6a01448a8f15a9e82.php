786f9b6305ace794fdafc3d1b6752174bdaf81ee
Egor Hod <76813379e@gmail.com>
2026-05-18 22:51:02 +0300

    first

---
+++ bin/phpMyAdmin5/tmp/twig/26/26acf62d05ad17b6a01448a8f15a9e82.php
@@ -0,0 +1,64 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* database/structure/collation_definition.twig */
+class __TwigTemplate_23e59027cbd3b71e5663f308fe5d6060 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<dfn title=\"";
+        echo twig_escape_filter($this->env, ($context["valueTitle"] ?? null), "html", null, true);
+        echo "\">";
+        echo twig_escape_filter($this->env, ($context["value"] ?? null), "html", null, true);
+        echo "</dfn>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "database/structure/collation_definition.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "database/structure/collation_definition.twig", "D:\\MAMP\\bin\\phpMyAdmin5\\templates\\database\\structure\\collation_definition.twig");
+    }
+}
